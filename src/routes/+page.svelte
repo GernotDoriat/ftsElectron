d818edb9a0382cf3ddf8c3d3c497f404a5d708c6
Gernot Doriat <gernot.doriat@web.de>
2024-07-18 12:26:04 +0200

    processFile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 	import { onMount } from "svelte";
-	import Papa from "papaparse";
 
 	onMount(init);
 	function init() {
@@ -13,116 +12,29 @@
 	}
 
 	$: costuraFiles = undefined;
-	$: reactOn(costuraFiles);
-	function reactOn() {
+	$: reactOnCosturaFiles(costuraFiles);
+	function reactOnCosturaFiles() {
 		if (costuraFiles && costuraFiles.length == 1) {
 			wolkensteinInput.value = "";
-			doParse("costura");
+			console.warn("reactOnCosturaFiles");
+			processFile(costuraFiles[0]);
 		}
 	}
 
 	$: wolkensteinFiles = undefined;
-	$: reactOn2(wolkensteinFiles);
-	function reactOn2() {
+	$: reactOnWolkensteinFiles(wolkensteinFiles);
+	function reactOnWolkensteinFiles() {
 		if (wolkensteinFiles && wolkensteinFiles.length == 1) {
 			costuraInput.value = "";
-			doParse("wolkenstein");
+			console.warn("reactOnWolkensteinFiles");
+			processFile(wolkensteinFiles[0]);
 		}
 	}
 
-	function doParse(type) {
-		console.log("doParse");
-		textRows = [];
-		let file = type == "costura" ? costuraFiles[0] : wolkensteinFiles[0];
-
-		// Parse local CSV file
-		Papa.parse(file, {
-			complete: function (results) {
-				console.log("Finished:", results.data);
-				if (Array.isArray(results.data) && results.data.length > 0) {
-					let rows = results.data;
-					let rowLength = rows[0].length;
-					let index = indexOfLastEmptyRow(rows, rowLength);
-					if (index > -1) {
-						let relevantRows = [];
-						for (let i = index + 1; i < results.data.length; i++) {
-							if (results.data[i].length == rowLength && results.data[i][0] == "") {
-								let relevantRow = [];
-								for (let j = 1; j < results.data[i].length; j++) {
-									relevantRow.push(results.data[i][j]);
-								}
-								relevantRows.push(relevantRow);
-							}
-						}
-						let items = [];
-						if (relevantRows.length == 4) {
-							let monthYear = "";
-							let day = "";
-							let time = "";
-							let commited = 0;
-							let needed = 0;
-
-							for (let i = 0; i < rowLength - 1; i++) {
-								// ohne YEAR!
-								if (relevantRows[0][i] != "") monthYear = relevantRows[0][i].split(" ")[0];
-								if (relevantRows[1][i] != "") day = relevantRows[1][i];
-								if (relevantRows[2][i] != "") time = relevantRows[2][i];
-								let commitedToNeeded = relevantRows[3][i].split("/");
-								commited = Number.parseInt(commitedToNeeded[0].trim());
-								needed = Number.parseInt(commitedToNeeded[1].trim());
-								let item = {
-									monthYear: monthYear,
-									day: day,
-									time: time,
-									commited: commited,
-									needed: needed,
-								};
-								items.push(item);
-							}
-						}
-						console.log("items", items);
-						let critcals = items.filter((i) => i.commited < i.needed);
-						console.log("critcals", critcals);
-						let wrkRows = [];
-						// ohne ort zeile
-						//wrkRows.push(`${rows[9][0]}:`);
-						// aber mit leerzeile
-						wrkRows.push("");
-						for (const critcal of critcals) {
-							if (type == "costura") wrkRows.push(`${critcal.day.trim()}. ${critcal.monthYear} ${critcal.time}`);
-							else
-								wrkRows.push(
-									`Für  ${critcal.day.trim()}. ${critcal.monthYear} ${
-										critcal.time
-									} Uhr benötigen wir noch ${critcal.needed - critcal.commited} Helfer.`,
-								);
-						}
-						textRows = wrkRows;
-					}
-				}
-			},
-		});
+	function processFile(file) {
+		console.warn("processFile", file);
 	}
 
-	function indexOfLastEmptyRow(rows, rowLength) {
-		let result = -1;
-		if (Array.isArray(rows) && rows.length > 0) {
-			for (let i = 0; i < rows.length; i++) {
-				if (rowEmpty(rows[i], rowLength)) result = i;
-			}
-		}
-		return result;
-	}
-
-	function rowEmpty(row, length) {
-		if (Array.isArray(row) && row.length == length) {
-			for (const cell of row) {
-				if (cell != "") return false;
-			}
-			return true;
-		}
-		return false;
-	}
 	$: textRows = [];
 	let costuraInput, wolkensteinInput;
 </script>
@@ -135,14 +47,14 @@
 </div>
 
 <div class="m-2 border-2 border-neutral-300">
-	<p class="m-3 text-3xl font-bold">Für Kassendienst La Costura</p>
+	<p class="m-3 text-3xl font-bold">La Costura</p>
 	<div class="p-4">
-		<input bind:this={costuraInput} class="text-2xl" type="file" accept=".csv" bind:files={costuraFiles} />
+		<input bind:this={costuraInput} class="text-2xl" type="file" bind:files={costuraFiles} />
 	</div>
 </div>
 
 <div class="m-2 border-2 border-neutral-300">
-	<p class="m-3 text-3xl font-bold">Für Helfer Wolkenstein Milonga</p>
+	<p class="m-3 text-3xl font-bold">Wolkenstein Milonga</p>
 	<div class="p-4">
 		<input bind:this={wolkensteinInput} class="text-2xl" type="file" accept=".csv" bind:files={wolkensteinFiles} />
 	</div>
