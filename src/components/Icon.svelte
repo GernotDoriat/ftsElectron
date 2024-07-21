323ed0e8af0ffe35b280fb8072437ecc2cc9266e
Gernot Doriat <gernot.doriat@web.de>
2024-07-21 11:27:04 +0200

    zws up and down

---
+++ src/components/Icon.svelte
@@ -0,0 +1,588 @@
+<script>
+	export let id = undefined;
+</script>
+
+{#if id == 'arrowDown'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-8 h-8"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
+		/>
+	</svg>
+{:else if id == 'arrowLeft'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="w-8 h-8"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		stroke-width="2"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"
+		/>
+	</svg>
+{:else if id == 'arrowRight'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="w-8 h-8"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		stroke-width="2"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
+		/>
+	</svg>
+{:else if id == 'arrowUp'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-8 h-8"
+	>
+		<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
+	</svg>
+{:else if id == 'checked40'}
+	<svg
+		class="w-8 h-8 text-white fill-current"
+		viewBox="0 0 40 40"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM16.6667 28.3333L8.33337 20L10.6834 17.65L16.6667 23.6166L29.3167 10.9666L31.6667 13.3333L16.6667 28.3333Z"
+		/>
+	</svg>
+{:else if id == 'chevronDown'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="w-8 h-8"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		stroke-width="2"
+	>
+		<path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+	</svg>
+{:else if id == 'chevronUp'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="w-8 h-8"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		stroke-width="2"
+	>
+		<path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
+	</svg>
+{:else if id == 'cloudUp1'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="h-8 w-8"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		stroke-width="1.5"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+		/>
+	</svg>
+{:else if id == 'circleStack'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-6 h-6"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"
+		/>
+	</svg>
+{:else if id == 'clock'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-8 h-8"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+		/>
+	</svg>
+{:else if id == 'cloudUp2'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="h-9 w-9"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		stroke-width="2"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+		/>
+	</svg>
+{:else if id == 'compare'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="w-8 h-8"
+		width="32"
+		height="32"
+		viewBox="0 0 32 32"
+	>
+		<path
+			d="M28,6H18V4a2,2,0,0,0-2-2H4A2,2,0,0,0,2,4V24a2,2,0,0,0,2,2H14v2a2,2,0,0,0,2,2H28a2,2,0,0,0,2-2V8A2,2,0,0,0,28,6ZM4,15h6.17L7.59,17.59,9,19l5-5L9,9,7.59,10.41,10.17,13H4V4H16V24H4ZM16,28V26a2,2,0,0,0,2-2V8H28v9H21.83l2.58-2.59L23,13l-5,5,5,5,1.41-1.41L21.83,19H28v9Z"
+			transform="translate(0)"
+		/>
+	</svg>
+{:else if id == 'docExport'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-8 h-8"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+		/>
+	</svg>
+	<!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m-6 3.75l3 3m0 0l3-3m-3 3V1.5m6 9h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75"
+        />
+    </svg> -->
+{:else if id == 'docImport'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-8 h-8"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m6.75 12l-3-3m0 0l-3 3m3-3v6m-1.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+		/>
+	</svg>
+
+	<!--  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m0-3l-3-3m0 0l-3 3m3-3v11.25m6-2.25h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75"
+        />
+    </svg> -->
+{:else if id == 'docUpdate'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-8 h-8"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3"
+		/>
+	</svg>
+{:else if id == 'exclamation40'}
+	<svg
+		class="w-8 h-8 text-white fill-current"
+		viewBox="0 0 40 40"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM21.6667 28.3333H18.3334V25H21.6667V28.3333ZM21.6667 21.6666H18.3334V11.6666H21.6667V21.6666Z"
+		/>
+	</svg>
+{:else if id == 'funnel'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="w-8 h-8"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		stroke-width="2"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
+		/>
+	</svg>
+{:else if id == 'grid'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="w-8 h-8"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		stroke-width="2"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
+		/>
+	</svg>
+{:else if id == 'hamburger'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="w-8 h-8"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		stroke-width="2"
+	>
+		<path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+	</svg>
+{:else if id == 'link'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="w-8 h-8"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		stroke-width="2"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+		/>
+	</svg>
+{:else if id == 'moon'}
+	<svg
+		class="w-8 h-8"
+		fill="none"
+		stroke="currentColor"
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
+		><path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="2"
+			d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+		/></svg
+	>
+{:else if id == 'lightning40'}
+	<svg
+		class="w-8 h-8 text-white fill-current"
+		viewBox="0 0 40 40"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z"
+		/>
+	</svg>
+{:else if id == 'pencil6'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-6 h-6"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+		/>
+	</svg>
+{:else if id == 'play'}
+	<svg
+		id="icon"
+		xmlns="http://www.w3.org/2000/svg"
+		class="w-8 h-8"
+		viewBox="0 0 32 32"
+		stroke-width="3"
+	>
+		<path
+			d="M7,28a1,1,0,0,1-1-1V5a1,1,0,0,1,1.4819-.8763l20,11a1,1,0,0,1,0,1.7525l-20,11A1.0005,1.0005,0,0,1,7,28ZM8,6.6909V25.3088L24.9248,16Z"
+			transform="translate(0)"
+		/>
+	</svg>
+{:else if id == 'plus'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="h-8 w-8"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		stroke-width="2"
+	>
+		<path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
+	</svg>
+{:else if id == 'publish'}
+	<svg id="icon" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+		<defs>
+			<style>
+				.cls-1 {
+					fill: none;
+				}
+			</style>
+		</defs>
+		<polygon points="6 18 7.41 19.41 15 11.83 15 30 17 30 17 11.83 24.59 19.41 26 18 16 8 6 18" />
+		<path d="M6,8V4H26V8h2V4a2,2,0,0,0-2-2H6A2,2,0,0,0,4,4V8Z" />
+		<rect
+			id="_Transparent_Rectangle_"
+			data-name="&lt;Transparent Rectangle&gt;"
+			class="cls-1"
+			width="32"
+			height="32"
+		/>
+	</svg>
+{:else if id == 'qrCode'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="w-8 h-8"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		stroke-width="2"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z"
+		/>
+	</svg>
+{:else if id == 'save'}
+	<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<g clip-path="url(#clip0_530_3815)">
+			<path
+				d="M27.71 9.29002L22.71 4.29002C22.6166 4.19734 22.5057 4.12401 22.3839 4.07425C22.2621 4.02448 22.1316 3.99926 22 4.00002H6C5.46957 4.00002 4.96086 4.21073 4.58579 4.5858C4.21071 4.96088 4 5.46958 4 6.00002V26C4 26.5305 4.21071 27.0392 4.58579 27.4142C4.96086 27.7893 5.46957 28 6 28H26C26.5304 28 27.0391 27.7893 27.4142 27.4142C27.7893 27.0392 28 26.5305 28 26V10C28.0008 9.86841 27.9755 9.73795 27.9258 9.61611C27.876 9.49427 27.8027 9.38346 27.71 9.29002ZM12 6.00002H20V10H12V6.00002ZM20 26H12V18H20V26ZM22 26V18C22 17.4696 21.7893 16.9609 21.4142 16.5858C21.0391 16.2107 20.5304 16 20 16H12C11.4696 16 10.9609 16.2107 10.5858 16.5858C10.2107 16.9609 10 17.4696 10 18V26H6V6.00002H10V10C10 10.5304 10.2107 11.0392 10.5858 11.4142C10.9609 11.7893 11.4696 12 12 12H20C20.5304 12 21.0391 11.7893 21.4142 11.4142C21.7893 11.0392 22 10.5304 22 10V6.41002L26 10.41V26H22Z"
+				fill="black"
+			/>
+		</g>
+		<defs>
+			<clipPath id="clip0_530_3815">
+				<rect width="32" height="32" fill="white" />
+			</clipPath>
+		</defs>
+	</svg>
+{:else if id == 'search'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-8 h-8"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+		/>
+	</svg>
+{:else if id == 'settings'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="w-8 h-8"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		stroke-width="2"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+		/>
+		<path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+	</svg>
+{:else if id == 'slash'}
+	<svg
+		class="flex-shrink-0 h-5 w-5 text-neutral-300"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="currentColor"
+		viewBox="0 0 20 20"
+		aria-hidden="true"
+	>
+		<path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+	</svg>
+{:else if id == 'sun'}
+	<svg
+		class="w-8 h-8"
+		fill="none"
+		stroke="currentColor"
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
+		><path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="2"
+			d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+		/></svg
+	>
+{:else if id == 'tool'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-8 h-8"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"
+		/>
+	</svg>
+{:else if id == 'undo'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="w-8 h-8"
+		width="32"
+		height="32"
+		viewBox="0 0 32 32"
+	>
+		<path
+			d="M20,10H7.8149l3.5874-3.5859L10,5,4,11,10,17l1.4023-1.4146L7.8179,12H20a6,6,0,0,1,0,12H12v2h8a8,8,0,0,0,0-16Z"
+		/>
+	</svg>
+{:else if id == 'upload'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="w-8 h-8"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		stroke-width="2"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
+		/>
+	</svg>
+{:else if id == 'user'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-8 h-8"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+		/>
+	</svg>
+{:else if id == 'userMinus'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-8 h-8"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M22 10.5h-6m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+		/>
+	</svg>
+{:else if id == 'userPlus'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-8 h-8"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+		/>
+	</svg>
+{:else if id == 'users'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-8 h-8"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+		/>
+	</svg>
+{:else if id == 'wasteBin'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="w-8 h-8"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+		/>
+	</svg>
+{:else if id == 'x'}
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		height="24px"
+		viewBox="0 0 24 24"
+		width="24px"
+		stroke="currentColor"
+	>
+		<path
+			d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"
+			fill="currentColor"
+		/>
+	</svg>
+{/if}
