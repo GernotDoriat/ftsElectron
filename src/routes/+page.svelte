46d2fc461d9441b8bab547925bde63c043c562c8
Gernot Doriat <gernot.doriat@web.de>
2024-07-22 16:25:53 +0200

    zws

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,9 +15,10 @@
 			textarea.addEventListener("select", logSelection);
 		}
 	}
+	$: currentSelection = undefined;
 	function logSelection(event) {
-		const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
-		console.log(selection);
+		currentSelection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
+		console.log(`SEL "${currentSelection}"`);
 	}
 
 	$: costuraFiles = undefined;
@@ -29,16 +30,26 @@
 		}
 	}
 
-	async function processFile(file) {
+	function reset() {
+		console.warn("reset");
+		currentSelection = undefined;
 		text1 = "";
 		text2 = "";
 		indexOfSearch = 0;
 		parts = [];
+	}
+
+	async function processFile(file) {
+		reset();
 		console.warn("processFile", file);
 		let result = await window.ipcElectron.invoke("extract-text", file.path);
-
 		processText(result.text);
 	}
+
+	function searchTextChange() {
+		processFile(costuraFiles[0]);
+	}
+
 	let searchText = "Beschluss";
 	let fileInput;
 
@@ -69,6 +80,11 @@
 		indexOfSearch++;
 		showParts();
 	}
+
+	function add() {
+		console.warn(`add "${currentSelection}"`);
+		//text1 = "GAGA";
+	}
 </script>
 
 <!-- 
@@ -88,7 +104,13 @@
 
 	<div class=" m-2">
 		<div class="px-4 w-full">
-			<input class="p-2 text-3xl w-full focus:outline-0" type="text" placeholder="Suchbegriff eingeben oder einsetzen..." bind:value={searchText} />
+			<input
+				class="p-2 text-3xl w-full focus:outline-0"
+				type="text"
+				placeholder="Suchbegriff eingeben oder einsetzen..."
+				bind:value={searchText}
+				on:input={searchTextChange}
+			/>
 		</div>
 	</div>
 
@@ -99,10 +121,18 @@
 			{:else}
 				<div class="w-8"></div>
 			{/if}
+
 			<p class="text-4xl font-semibold">{indexOfSearch + 1}</p>
 
 			{#if indexOfSearch < parts.length - 2}
 				<button class=" text-xl font-bold bg-orange-600 text-white" on:click={moveDown}> <Icon id="arrowDown" /></button>
+			{:else}
+				<div class="w-8"></div>
+			{/if}
+		</div>
+		<div class="mx-2 my-3 h-10 flex gap-2">
+			{#if currentSelection}
+				<button class="text-xl font-bold bg-orange-600 text-white" on:click={add}><Icon id="plus" /></button>
 			{:else}
 				<div class="w-8"></div>
 			{/if}
